0ada63304dc31c4315ce8d068b41132141a69249
NathanWaddell1211 <waddellnathan1211@gmail.com>
2020-09-03 22:21:31 -0500

    Fixing mobile styling for the header

--- presentation/components/header/header.styles.ts
+++ presentation/components/header/header.styles.ts
@@ -1,14 +1,22 @@
 import styled from 'styled-components'
 
 export const HeaderWrapper = styled.div`
-	position: relative;
-	z-index: 100;
-	opacity: 0.7;
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
+`
+
+export const HeaderOuterWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
 	background-color: #dedede;
+	position: relative;
+	z-index: 100;
+	opacity: 0.7;
 	padding: 2rem;
+	@media (max-width: 768px) {
+		padding: 0.785rem 1.25rem;
+	}
 `
 
 export const HeaderLogo = styled.div`
@@ -22,6 +30,31 @@ export const HeaderMenuItems = styled.div`
 	> a {
 		text-decoration: none;
 		color: #000;
+		&:hover {
+			color: #28bafd;
+		}
+	}
+	@media (max-width: 768px) {
+		display: none;
+	}
+`
+
+export const IconWrapper = styled.div`
+	@media (min-width: 770px) {
+		display: none;
+	}
+`
+
+export const MobileHeaderMenuItems = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-evenly;
+	margin-top: 1rem;
+	> a {
+		text-decoration: none;
+		color: #000;
+		padding: 0.5rem 0;
+		border-top: 1px solid black;
 		&:hover {
 			color: #28bafd;
 		}
--- presentation/components/header/index.tsx
+++ presentation/components/header/index.tsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as Styled from './header.styles'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
+	const [navMenuIsOpen, setNavMenuIsOpen] = useState(false)
+	const toggleNav = () => {
+		setNavMenuIsOpen(!navMenuIsOpen)
+	}
 	return (
 		<>
-			<Styled.HeaderWrapper>
-				<Styled.HeaderLogo>TIRESIAS</Styled.HeaderLogo>
-				<Styled.HeaderMenuItems>
-					<a href="#">Charts</a>
-					<a href="#">Portfolio</a>
-					<a href="#">Account</a>
-				</Styled.HeaderMenuItems>
-			</Styled.HeaderWrapper>
+			<Styled.HeaderOuterWrapper>
+				<Styled.HeaderWrapper>
+					<Styled.HeaderLogo>TIRESIAS</Styled.HeaderLogo>
+					<Styled.HeaderMenuItems>
+						<a href="#">Charts</a>
+						<a href="#">Portfolio</a>
+						<a href="#">Account</a>
+					</Styled.HeaderMenuItems>
+					<Styled.IconWrapper>
+						<FontAwesomeIcon icon={faBars} onClick={() => toggleNav()} />
+					</Styled.IconWrapper>
+				</Styled.HeaderWrapper>
+				{navMenuIsOpen && (
+					<Styled.MobileHeaderMenuItems>
+						<a href="#">Charts</a>
+						<a href="#">Portfolio</a>
+						<a href="#" style={{ borderBottom: '1px solid black' }}>
+							Account
+						</a>
+					</Styled.MobileHeaderMenuItems>
+				)}
+			</Styled.HeaderOuterWrapper>
 		</>
 	)
 }
