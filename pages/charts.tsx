8105179d32b89947b4eee92dcc0b9d93c6e1c1a2
unknown <waddellnathan1211@gmail.com>
2020-09-25 22:13:02 -0500

    Adding header and global theme to chart page

--- pages/charts.tsx
+++ pages/charts.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import Charts from '../presentation/components/charts'
+import { GlobalStyle } from '../presentation/components/global-theme'
+import Header from '../presentation/components/header'
 
 const ChartsPage: React.FC = () => {
-	return <Charts />
+	return (
+		<>
+			<GlobalStyle />
+			<Header />
+			<Charts />
+		</>
+	)
 }
 
 export default ChartsPage
