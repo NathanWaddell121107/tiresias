9d9366e6a4ac73a6a1d773cdea606bb13886aba0
NathanWaddell1211 <waddellnathan1211@gmail.com>
2020-10-01 22:05:09 -0500

    Fixing issue

--- pages/charts.tsx
+++ pages/charts.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Charts from '../presentation/components/charts'
 import { GlobalStyle } from '../presentation/components/global-theme'
-import Header from '../presentation/components/header'
 
 const ChartsPage: React.FC = () => {
 	return (
 		<>
 			<GlobalStyle />
-			<Header />
 			<Charts />
 		</>
 	)
