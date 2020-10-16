e890da9feb7995ab5eb4eefdc376e12e359883fd
unknown <waddellnathan1211@gmail.com>
2020-10-15 22:16:12 -0500

    Adding a main page title

--- presentation/components/main-page/index.tsx
+++ presentation/components/main-page/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import Header from '../header'
 import Particles, { IParticlesParams } from 'react-particles-js'
 import { getPolygonConfig } from '../../helpers/polygon-config'
+import * as Styled from './main-page.styles'
 
 const MainPage = () => {
 	return (
 		<>
 			<Particles style={{ position: 'fixed', top: 0 }} params={getPolygonConfig() as IParticlesParams} />
+			<Styled.MainPageTitle>
+				<h1>CRYPTOCURRENCY ANALYSIS</h1>
+				<h1>SIMPLIFIED</h1>
+			</Styled.MainPageTitle>
 		</>
 	)
 }
---
+++ presentation/components/main-page/main-page.styles.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const MainPageTitle = styled.div`
+    margin: 15% auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    > h1 {
+        z-index: 1;
+        font-size: 2.2rem;
+        color: #fff;
+        text-align: center;
+        padding: 0.75rem;
+    }
+`
